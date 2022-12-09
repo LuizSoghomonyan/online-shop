652f0c9f397ed742453dc3e2d36d046ddc5c891b
Luiza Soghomonyan <luiza.soxhomonyan@gmail.com>
2022-12-10 03:17:46 +0400

    firebase: add login page

---
+++ src/app/auth/models/user.ts
@@ -0,0 +1,15 @@
+export interface User {
+  id?: string,
+  displayName: string,
+  email: string,
+  emailVerified: boolean
+}
+
+export function emptyUser(): User{
+  return <User>{
+    email: '',
+    displayName: '',
+    id: '',
+    emailVerified: false
+  };
+}
