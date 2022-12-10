7fbe4139a097f27a880517db428287b4c97e06a6
Luiza Soghomonyan <luiza.soxhomonyan@gmail.com>
2022-12-11 03:36:28 +0400

    test for husky

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css'],
 })
-export class TestComponent {}
+export class TestComponent implements OnInit {
+  ngOnInit() {
+
+
+  }
+}
