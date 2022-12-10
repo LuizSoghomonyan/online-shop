3997c0b0382bad7a09e3b1d4a026adcd3a54bff4
Luiza Soghomonyan <luiza.soxhomonyan@gmail.com>
2022-12-11 03:51:42 +0400

    test for husky 7

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from "@angular/core";
 export class TestComponent implements OnInit {
   ttt: number=12312;
   ttt22: number=12312;
+  ttt212: number=12312;
+  ttt2312: number=12312;
   ngOnInit() {
 
 
